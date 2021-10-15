51b97fb1044d0e6c4229a9c441763c302e8c8349
puliwang@cxist.com <puliwang@cxist.com>
2021-10-15 17:27:33 +0800

    2

--- ng-app/src/app/app-routing.module.ts
+++ ng-app/src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { APP_BASE_HREF } from '@angular/common';
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: '**', redirectTo: 'home' }
+    { path: '**', redirectTo: 'home' },
 ];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+    exports: [RouterModule],
+    // @ts-ignore
+    providers: [{ provide: APP_BASE_HREF, useValue: window.__POWERED_BY_QIANKUN__ ? '/ng-app' : '/' }]
 })
 export class AppRoutingModule { }
